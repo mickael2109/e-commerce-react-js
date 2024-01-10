3e035c22b83bb5ecc77063d2e2bc1ddede94512b
Mickael <mickaelrkt20@gmail.com>
2024-01-10 22:30:16 +0300

    Responsive application, preparation and installation of Redux.

---
+++ src/lib/actions.js
@@ -0,0 +1,33 @@
+/*
+ * action types
+ */
+export const actions = {
+    ADD_TO_CART: 'ADD_TO_CART',
+    UPDATE_CART: 'UPDATE_CART',
+    REMOVE_FROM_CART: 'REMOVE_FROM_CART'
+}
+
+/*
+ * actions creators
+ */
+const uid = () => Math.random().toString(11).slice(2)
+export function addtoCart(item, quantity){
+    return{
+        type: actions.ADD_TO_CART,
+        payload: {id: uid, quantity: quantity, details: item}
+    }
+}
+
+export function updateCart(item, quantity){
+    return{
+        type: actions.UPDATE_CART,
+        payload: {item: item, quantity}
+    }
+}
+
+export function removeCart(item){
+    return{
+        type: actions.REMOVE_FROM_CART,
+        payload: item
+    }
+}
---
+++ src/lib/reducers.js
@@ -0,0 +1,25 @@
+import { actions } from "./actions"
+
+const initialState = {
+    items : []
+}
+
+export default function onlineStoreApp(state = initialState, action){
+    switch(action.type){
+        case actions.ADD_TO_CART : return Object.assign({}, state, { items : [...state.items, action.payload]})
+        case actions.UPDATE_CART : return Object.assign({}, state, {
+            items : state.items.map(item => {
+                return item.id === action.payload.item.id ? 
+                    Object.assign({}, item, {
+                        quantity: action.payload.quantity
+                    }) : item ;
+            })
+        })
+        case actions.REMOVE_FROM_CART : return Object.assign({}, state, {
+            items: state.items.filter(item => {
+                return item.id != action.payload.id
+            })
+        })
+        // default :return item
+    }
+}
