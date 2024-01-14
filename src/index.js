c82d855122ab3c37274da4b028a18b229930bf3f
Mickael <mickaelrkt20@gmail.com>
2024-01-14 07:49:52 +0300

    a

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import 'jquery';
 import 'popper.js';
 import { Provider } from 'react-redux';
 import { store } from './lib/store';
-// import { addtoCart } from './lib/actions';
 import { AppContainer } from './views/containers';
 
 
@@ -31,7 +30,4 @@ root.render(
   </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
